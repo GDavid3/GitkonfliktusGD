9e8083ffd85105919bd5a1d85bda97f1cf3f83bc
Gulyás Dávid <72173665289@szily.lan>
2022-03-22 09:40:32 +0100

    alap projekt

---
+++ Gitkonfliktuskezeles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gitkonfliktuskezeles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
